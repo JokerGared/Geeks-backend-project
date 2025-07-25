ce8ac20fbf195ed7377c28b64430f6ec344e8ee6
Oleh <therkasoa@gmail.com>
2025-07-25 19:42:15 +0300

    deleted test things

--- src/routers/index.js
+++ src/routers/index.js
@@ -2,12 +2,10 @@ import { Router } from 'express';
 import authRouter from './auth.js';
 import articlesRouter from './articles.js';
 import userRouter from './users.js';
-import testRouter from './dbtest.js';
 
 const router = Router();
 router.use(articlesRouter);
 router.use(authRouter);
 router.use(userRouter);
-router.use(testRouter);
 
 export default router;
